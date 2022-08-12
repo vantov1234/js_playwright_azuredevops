814822d92772cf7c8704436a20c8a09930ad4bdf
vantov1234 <valentinantov@yahoo.com>
2022-08-12 14:44:04 +0300

    init

--- test.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test')
-
-test('Simple test', async ({ request }) => {
-    const baseURL = 'https://reqres.in/api'
-
-    const response = await request.get(baseURL + '/users/3')
-    expect(response.status()).toBe(200)
-    
-})
